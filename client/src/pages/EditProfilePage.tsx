669b58964fd0d9cc6fe1bfd207e49fef05aa1c2c
swaroop-12 <swaroop.k.g49@gmail.com>
2024-12-08 18:30:19 +0530

    error solved

--- client/src/pages/EditProfilePage.tsx
+++ client/src/pages/EditProfilePage.tsx
@@ -9,7 +9,7 @@ import Loader from '../components/Loader';
 import { toast } from 'sonner';
 import { useNavigate } from 'react-router-dom';
 import { config } from '../utils/config';
-import bgout from './auth/assets/profile1.webp';
+
 
 
 const EditProfilePage = () => {
