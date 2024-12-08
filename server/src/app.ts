2b436d7399f84dfaafa3c717b351c6ab970dd0c3
swaroop-12 <swaroop.k.g49@gmail.com>
2024-12-08 20:53:11 +0530

    updated3

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,13 +4,17 @@ import morgan from 'morgan';
 import rootRouter from './routes';
 import notFound from './middlewares/notFound';
 import globalErrorHandler from './middlewares/globalErrorhandler';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(cors({ origin: ['http://localhost:5173', 'inovatory-management.vercel.app'] }));
+app.use(cors({ origin: ['process.env.FRONTEND_URL', 'inovatory-management.vercel.app'] }));
 
 // application routes
 app.use('/api/v1', rootRouter);
